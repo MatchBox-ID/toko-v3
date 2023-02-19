6d7aa7226de14c0ae49f677371d3c89254f1884e
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2023-02-19 16:14:04 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+console.log('MATCHBOX SERVER')
+console.log('Berjalan pada database MySql; hostname:matchbox port:localhost:9908')
+console.log('website ini dibuat pada tanggal 4 bulan february tahun 2023')
+
+console.log('masalah konfigurasi pada port localhost:9908')
