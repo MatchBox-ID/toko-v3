db0e97b9d2bcda8ace4f08eed1009ac75acde6c3
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2023-02-24 15:09:05 +0700

    Update validation.js

--- validation.js
+++ validation.js
@@ -6,6 +6,10 @@ var password = document.forms['form']['password']
 var usser_error = document.getElementById('usser_error');
 var pass_error = document.getElementById('pass_error');
 
+ussername.addEventListener('textInput', ussername_Verify);
+password.addEventListener('textInput', password_Verify);
+
+
    function validate(){
        if (ussername.value.length < 9) {
            ussername.style.border = "1px solid red";
@@ -18,4 +22,18 @@ var pass_error = document.getElementById('pass_error');
            password.focus();
            return false;
    }
+}
+function ussername_Verify(){
+    if (ussername.value.length >= 8) {
+        ussername.style.border = "1px solid silver";
+        usser_error.style.display = "none";
+        return true;
+    }
+}
+function password_Verify(){
+    if (password.value.length >= 8) {
+        password.style.border = "1px solid silver";
+        pass_error.style.display = "none";
+        return true;
+    }
 }
