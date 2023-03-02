7c22f4a2c21f6c37cb7e4feb343498191b286e42
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2023-03-02 20:58:35 +0700

    update

--- bot/server.js
+++ bot/server.js
@@ -1,1 +1,51 @@
-
+       const apikey = ("https://quillbot.com/apikey=freeapi")
+       const member = ("https://rest-api.xyz/count-api/viewers-member")
+       const bot = ("https://mcafee.com/security-bot/%20apikey=(mbot)")
+       const server = ("bot.py")
+       const home = ("index.html")
+       const login = ("login.html")
+       const regristrasi = ("regristrasi.html")
+       const maker-app = ("https://unity3d.com/maker-bot/%20checkServer=(unityfreeserver)")
+       const googleapis = ("https://googleapis.com")
+       {
+         if trouble.("login")
+         if server("server.js")
+       }
+       {
+         const quillbot ("https://quillbot.com/%20apikey={freeapis}")
+         const login ("py.server.js")
+       }
+            {
+              if "ok" = [
+                shadow.only(serverOnly),{login},{quillbot};
+                makes.create.theApp{"maker-app"};
+                echo={"security is ok"}]
+               break
+            }
+            {
+              if "open-ai" = [
+                shadow.openAi(notopenAi.server),{openAi},{makes-app};
+                the-server{"server sedang down harap ulangi lagi setelah 30 detik"}
+                echo={"the-server"}
+               break
+                ]
+            }
+           
+            {
+              if command{"start"} = [
+                echo1 = ("memulai indeks...")
+                echo2 = ("aplikasi bot sedang dimulai")
+                echo3 = ("loading progress")
+                      = ("10%")
+                      = ("20%")
+                      = ("30%")
+                      = ("40%")
+                      = ("50%")
+                      = ("75%")
+                      = ("90%")
+                      = ("100%")
+                echo4 = ("progress berhasil!")
+                if false = 
+                      = ("progress gagal, ilangi lagi nanti")
+                ]
+            }
