54a4737d70f8e4931b86c75ba1d737f82e7e7a1b
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2023-03-03 10:25:06 +0700

    update

--- bot/server2.js
+++ bot/server2.js
@@ -1,1 +1,20 @@
-
+//setting website 
+connect to = ("https://googleapis.com/apikey={freeapi}")
+   {
+     owner.web = ("matchbox")
+     owner.website = ("matchbox")
+     owner.https = ("https://matchstore.vercel.app")
+     proxy.security = ("https://icann.lookup.com")
+     ssl.dns = ("https://cloudflare.com/matchbox/matchbox.my.id/dns-record/create-dns/vmimp"),{domain.dns}
+   }
+   {
+     {
+       {
+         {
+           const api-key {"https://matchstore.vercel.app"}
+           create-api {"https://makesapis.com/create/vps2022"}
+         }
+       }
+     }
+   }
+   
